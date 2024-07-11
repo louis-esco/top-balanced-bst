5f78f9478ee0701f755f0ef570e50d7e5ef804b4
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-11 14:29:50 +0200

    Adds print tree method

--- tree.js
+++ tree.js
@@ -7,6 +7,7 @@ export default class Tree {
   }
 
   buildTree(array) {
+    console.log(array);
     if (array.length === 0) {
       return null;
     }
@@ -19,5 +20,22 @@ export default class Tree {
     const node = new Node(root, this.buildTree(left), this.buildTree(right));
 
     return node;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 }

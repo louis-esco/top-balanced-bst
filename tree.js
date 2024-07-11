7539584a969f442b771a125f18847f93e7431f20
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-11 16:49:31 +0200

    Adds deleteItem method

--- tree.js
+++ tree.js
@@ -3,11 +3,12 @@ import Node from "./node.js";
 export default class Tree {
   constructor(array) {
     const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    console.log(sortedArray);
+    console.log(sortedArray.length);
     this.root = this.buildTree(sortedArray);
   }
 
   buildTree(array) {
-    console.log(array);
     if (array.length === 0) {
       return null;
     }
@@ -22,18 +23,48 @@ export default class Tree {
     return node;
   }
 
-  insert(value, root = this.root) {
-    if (root === null) {
-      root = new Node(value);
-      return root;
+  insert(value, node = this.root) {
+    if (node === null) {
+      node = new Node(value);
+      return node;
     }
 
-    if (value < root.data) {
-      root.left = this.insert(value, root.left);
-    } else if (value > root.data) {
-      root.right = this.insert(value, root.right);
+    if (value < node.data) {
+      node.left = this.insert(value, node.left);
+    } else if (value > node.data) {
+      node.right = this.insert(value, node.right);
     }
-    return root;
+    return node;
+  }
+
+  deleteItem(value, node = this.root) {
+    if (node === null) return node;
+
+    if (value < node.data) {
+      // Finds the node in the tree and runs the function recursively
+      node.left = this.deleteItem(value, node.left);
+    } else if (value > node.data) {
+      node.right = this.deleteItem(value, node.right);
+    } else {
+      if (node.left === null) {
+        // If node has single on no child, then this child becomes the child of the previous node
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      } else {
+        // If node has two children
+        // Identify the closest superior node
+        let closestNode = node.right;
+        while (closestNode.left !== null) {
+          closestNode = closestNode.left;
+        }
+        // Plug the node to its right to the following node
+        node.right = this.deleteItem(closestNode.data, node.right);
+        // Return closest node data with deleted node links
+        return new Node(closestNode.data, node.left, node.right);
+      }
+    }
+    return node;
   }
 
   prettyPrint(node, prefix = "", isLeft = true) {

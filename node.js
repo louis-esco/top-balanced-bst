682c27ded666c0793911c5144107754b08e7fb61
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-11 14:31:46 +0200

    Adds null default value

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 export default class Node {
-  constructor(data, left, right) {
+  constructor(data, left = null, right = null) {
     this.data = data;
     this.left = left;
     this.right = right;
